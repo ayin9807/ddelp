0d310f8494b402a7d6b0e0f590813f969758dfed
Annie Yin <wen.yin@duke.edu>
2018-04-26 14:43:46 -0400

    added nav bar

--- ddelp/src/App.vue
+++ ddelp/src/App.vue
@@ -1,21 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div id="nav-bar"> 
+        <h3>DDelp</h3>
+        <input id="search-bar">
+        <div id="log-in">Log in</div>
+        <div id="sign-up">Sign up</div>
+    </div>
   </div>
 </template>
 
@@ -38,6 +28,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+    
+#nav-bar {
+    display: flex;
+    flex-direction: row;
 }
 
 h1, h2 {
