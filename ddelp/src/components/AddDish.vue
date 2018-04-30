312884ece40aa7a843d8f993027e5234a7a6a09f
Carolyn Sun <carolyn.sun@duke.edu>
2018-04-29 20:11:31 -0400

    add psa duplicate in adddish

--- ddelp/src/components/AddDish.vue
+++ ddelp/src/components/AddDish.vue
@@ -4,6 +4,7 @@
     <v-card width="600px">
         <v-container fluid>
         <v-card-title><h1 style="color: darkgray;">Add Dish</h1></v-card-title>
+        <h3>To prevent duplicate dishes, please search for your dish in the navbar first!</h3>
         <v-layout row justify-center>
             <v-text-field id="cardTitle" v-model="newDishName" placeholder="Dish Name"></v-text-field>
         </v-layout>
